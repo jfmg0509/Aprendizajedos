ebe172b13f05a7755a5106d6d16fe8ed9eaaf642
Juan Francisco <jfmg0509@gmail.com>
2025-06-13 22:00:56 -0500

    creacion

---
+++ src/Circulo.java
@@ -0,0 +1,24 @@
+public class Circulo {
+    private double radio;
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    public double area() {
+        return Math.PI * Math.pow(radio, 2);
+    }
+
+    public double perimetro() {
+        return 2 * Math.PI * radio;
+    }
+    
+}
